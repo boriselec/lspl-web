b48fb1a1bdd37daff1c1968b4d823ed9a3185f96
boris puzikov <boris.puzikov@gmail.com>
2018-01-18 01:47:39 +0300

    подключен lspl
    new нативная библиотека под linux
    new java обертка lspl
    new справочники

--- src/main/java/com/boriselec/lspl/controllers/MainController.java
+++ src/main/java/com/boriselec/lspl/controllers/MainController.java
@@ -3,11 +3,15 @@ package com.boriselec.lspl.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.lspl.text.Text;
 
 @Controller
 public class MainController {
     @RequestMapping(value="/main", method = RequestMethod.GET, produces = "text/html;charset=UTF-8")
+    @ResponseBody
     public String viewHome(){
-        return "main";
+        Text test = Text.create("Мама мыла раму.");
+        return test.getWords().iterator().next().speechPart.getTitle();
     }
 }
